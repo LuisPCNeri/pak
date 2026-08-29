13ddaca8d89ce25dc20f700df5c31233289dfd41
LuisPCNeri <luispedrocneri@gmail.com>
2026-08-29 21:11:03 +0100

    added sort by name and sort by size functions, changed the fuzzy find function to return a mutable array of packages.

--- src/util/fuzzy.zig
+++ src/util/fuzzy.zig
@@ -3,7 +3,7 @@ const std: type = @import("std");
 const db: type   = @import("../db/database.zig");
 const pckg: type = db.Package;
 
-fn filter_orphans_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]const pckg {
+fn filter_orphans_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]pckg {
 
     var matches = try std.ArrayList(pckg).initCapacity(temp_aloc, pckgs.len);
     errdefer matches.deinit(temp_aloc);
@@ -15,7 +15,7 @@ fn filter_orphans_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]con
     return matches.items;
 }
 
-fn filter_deps_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]const pckg {
+fn filter_deps_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]pckg {
 
     var matches = try std.ArrayList(pckg).initCapacity(temp_aloc, pckgs.len);
     errdefer matches.deinit(temp_aloc);
@@ -27,10 +27,10 @@ fn filter_deps_only(temp_aloc: std.mem.Allocator, pckgs: []const pckg) ![]const 
     return matches.items;
 }
 
-pub fn fuzzy_find(temp_aloc: std.mem.Allocator, prefix: []const u8, pckgs: []const pckg) ![]const pckg {
+pub fn fuzzy_find(temp_aloc: std.mem.Allocator, prefix: []const u8, pckgs: []const pckg) ![]pckg {
 
     if(prefix.len == 0 or std.mem.eql(u8, prefix, "\n") or std.mem.eql(u8, prefix, " ")) {
-        return pckgs;
+        return try temp_aloc.dupe(pckg, pckgs);
     }
 
     var matches = try std.ArrayList(pckg).initCapacity(temp_aloc, pckgs.len);
@@ -50,4 +50,22 @@ pub fn fuzzy_find(temp_aloc: std.mem.Allocator, prefix: []const u8, pckgs: []con
     }
 
     return matches.items;
+}
+
+pub fn sort_by_pckg_size(list: []db.Package) void {
+
+    std.mem.sort(db.Package, list, {}, struct {
+        fn greaterThan(_: void, a: db.Package, b: db.Package) bool {
+            return a.size > b.size;
+        }
+    }.greaterThan);
+}
+
+pub fn sort_by_pckg_name(list: []db.Package) void {
+
+    std.mem.sort(pckg, list, {}, struct {
+        fn lessThan(_: void, a: db.Package, b: db.Package) bool {
+            return std.mem.lessThan(u8, a.name, b.name);
+        }
+    }.lessThan);
 }
